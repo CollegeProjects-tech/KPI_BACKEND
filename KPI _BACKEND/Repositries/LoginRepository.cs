83c3c7871cd32f5c5e76b2270e614f6e5ef86eb5
Prathmesh Kanekar <prathmeshkanekar2002@gmail.com>
2025-05-08 21:37:05 +0530

    create login controller

---
+++ KPI _BACKEND/Repositries/LoginRepository.cs
@@ -0,0 +1,46 @@
+﻿using Dapper;
+using KPI_BACKEND.Constants;
+using KPI_BACKEND.Contest;
+using KPI_BACKEND.Interfaces;
+using Models;
+using System.Data;
+
+namespace KPI_BACKEND.Repositries
+{
+    public class LoginRepository : ILogin
+    {
+        private readonly DapperContext context;
+
+        public LoginRepository(DapperContext context)
+        {
+            //_dbContext = dBContext;
+            this.context = context;
+        }
+
+        public async Task<tbl_Login> Login(tbl_Login login)
+        {
+            try
+            {
+                var query = "sp_Login";
+
+
+                var parameters = new DynamicParameters();
+                parameters.Add("@Action", QueryConstant.Login);
+                parameters.Add("@username", login.username);
+                parameters.Add("@password", login.password);
+
+
+
+                using (var connection = context.CreateConnection())
+                {
+                    var result = await connection.QuerySingleOrDefaultAsync<tbl_Login>(query, parameters, commandType: CommandType.StoredProcedure);
+                    return result;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
--- Models/obj/Debug/net8.0/Models.AssemblyInfo.cs
+++ Models/obj/Debug/net8.0/Models.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Models")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf9af62a212fc257743030be7a80550866f7ba21")]
 [assembly: System.Reflection.AssemblyProductAttribute("Models")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Models")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
